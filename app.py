5a45456f0b0f443e8dfe91e66158d0912328e215
B220067 <140996245+B220067@users.noreply.github.com>
2025-06-30 22:36:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -183,4 +183,6 @@ def best():
     best_results = {k: list(v) if v[0] is not None else ["", ""] for k, v in best_results.items()}
     return jsonify(best_results)
 if __name__ == "__main__":
-    app.run(debug=True)
+    if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
